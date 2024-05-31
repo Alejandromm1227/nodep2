5682f4a8ce82f350fc4eafe4ac63b46b202d78d2
Alejandromm1227 <160982660+Alejandromm1227@users.noreply.github.com>
2024-05-30 21:48:32 -0500

    Backend terminado solo queda agregar detalles del esquema

--- src/esquema/esquemaPais.js
+++ src/esquema/esquemaPais.js
@@ -5,6 +5,9 @@ const esquemaPais = new Schema(
         nombre : {type: String, required: true},
         idioma : {type: String, required: true},
         moneda : {type: String, required: true},
+        independencia : {type: Date, required: true},
+        lema : {type: String, required: true},
+        lugarPorSuperficie : {type: Number, required: true}
     })
 
 export default model ("Paises", esquemaPais);
